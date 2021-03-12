6adea27657d2b37a2466ec30f02aeb4eadaf962b
David <dvorakdavid7@gmail.com>
2021-03-12 09:30:46 +0100

    removed logs, food desc text changed

--- public/dist/app.js
+++ public/dist/app.js
@@ -3,21 +3,21 @@ var paralax1 = document.querySelector("#paralax1");
 var paralax2 = document.querySelector("#paralax2");
 window.addEventListener("scroll", function () {
     var offset = window.pageYOffset;
-    console.log(offset);
+    // console.log(offset);
     paralax1.style.backgroundPositionY = offset * 0.5 + "px";
 });
 window.addEventListener("scroll", function () {
     var offset = window.pageYOffset;
-    console.log(offset);
+    // console.log(offset);
     paralax2.style.backgroundPositionY = offset * 0.3 + "px";
 });
 var hand = document.querySelector(".hours");
 window.addEventListener("scroll", function () {
     // let offset = window.pageYOffset;
     // paralax.style.transform = "rotateZ(" + deg);
-    console.log(window.pageYOffset);
+    // console.log(window.pageYOffset);
     // console.log((window.pageYOffset - 253) * 0.78)
-    hand.style.transform = "rotateZ(" + (window.pageYOffset * 0.4) + "deg)";
+    hand.style.transform = "rotateZ(" + window.pageYOffset * 0.4 + "deg)";
 });
 var hamburgerBTN = document.querySelector(".fa-bars");
 var toggleBTN = document.querySelector(".fa-times");
--- public/src/app.ts
+++ public/src/app.ts
@@ -3,13 +3,13 @@ const paralax2 = document.querySelector("#paralax2") as HTMLDivElement;
 
 window.addEventListener("scroll", function () {
     let offset: number = window.pageYOffset;
-    console.log(offset);
+    // console.log(offset);
     paralax1.style.backgroundPositionY = offset * 0.5 + "px";
 })
 
 window.addEventListener("scroll", function () {
     let offset: number = window.pageYOffset;
-    console.log(offset);
+    // console.log(offset);
     paralax2.style.backgroundPositionY = offset * 0.3 + "px";
 })
 
@@ -18,9 +18,9 @@ let hand = document.querySelector(".hours") as HTMLDivElement;
 window.addEventListener("scroll", function () {
     // let offset = window.pageYOffset;
     // paralax.style.transform = "rotateZ(" + deg);
-    console.log(window.pageYOffset);
+    // console.log(window.pageYOffset);
     // console.log((window.pageYOffset - 253) * 0.78)
-    hand.style.transform = "rotateZ(" + (window.pageYOffset * 0.4) + "deg)"
+    hand.style.transform = `rotateZ(${window.pageYOffset * 0.4}deg)`
 })
 
 
